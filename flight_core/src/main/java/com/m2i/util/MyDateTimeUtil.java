3af3822f07d8e74c2a201152fdf3d1ec4191a455
didier <didier@ici_ou_labas.fr>
2018-05-31 14:00:23 +0200

    nouvelle version

--- flight_core/src/main/java/com/m2i/util/MyDateTimeUtil.java
+++ flight_core/src/main/java/com/m2i/util/MyDateTimeUtil.java
@@ -4,13 +4,12 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class MyDateTimeUtil {
     public static Date dateFromString(String strDate){
     	Date date = null;
 		try {
-			DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+			DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 			date = format.parse(strDate);
 		} catch (ParseException e) {
 			System.err.println(e.getMessage());
